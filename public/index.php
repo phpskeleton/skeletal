0862bc95d7b8bb9a93fc1b186baeb0ffa036b523
Ben Hirst <ben@Bens-MacBook-Pro.local>
2022-05-22 03:21:02 +0100

    update public/index.php test file

--- public/index.php
+++ public/index.php
@@ -7,8 +7,24 @@ use Skeletal\Support\Response;
 
 $app = Skeletal::getInstance();
 
-$app->handleRequest(function (Response $response, Log $log) {
-    return Response::json([
-        'status' => 'success'
-    ]);
+$app->handleRequest(function (Response $response) {
+
+    $collection = collect();
+
+    debug('----------');
+
+    $collection->set('address.street_address', '123 Manchester Road');
+    $collection->set('address.city', 'Manchester');
+    $collection->set('address.postcode.a', 'a');
+
+    $collection->set('address.postcode.b', 'b');
+
+    $collection->set('address.postcode.a', 'c');
+
+    $collection->set('test', 5);
+    $collection->unset('test');
+
+    $collection->merge(['test 1'], ['test 2'], ['test 3'])->values();
+
+    return Response::json($collection);
 });
