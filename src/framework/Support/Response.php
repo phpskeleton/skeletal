c652bebeb075b09dcb1dfedf2f778918d9d95fce
Ben Hirst <ben@Bens-MacBook-Pro.local>
2022-05-22 03:23:25 +0100

    Updated Skeletal\Support\Response

--- src/framework/Support/Response.php
+++ src/framework/Support/Response.php
@@ -3,19 +3,31 @@
 namespace Skeletal\Support;
 
 use Stringable;
+use Throwable;
 
 class Response implements Stringable
 {
 
-    public static function json(array $body): string
+    public static function json(array|Collection $body): string
     {
         header('Content-Type: ' . 'application/json');
-        return json_encode($body);
+        return json_encode($body instanceof Collection ? $body->getArrayCopy() : $body);
     }
 
-    public static function send(string $body): void
+    public static function send(string|Stringable $body): void
     {
         echo $body;
+    }
+
+    public static function sendError(Throwable $e)
+    {
+        echo static::json([
+            'message' => $e->getMessage(),
+            'code' => $e->getCode(),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'trace' => $e->getTraceAsString()
+        ]);
     }
 
     public function __toString(): string
